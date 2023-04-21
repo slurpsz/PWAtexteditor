9948408205c41adcac89ebc44ae8820383453e7a
slurpeesz <khtbee@gmail.com>
2023-04-21 00:45:58 -0700

    added logic in database and edited script

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+// console.error('putDb not implemented');
+    console.log("PUT to the database");
+    const jateDb = await openDB("jate", 1);
+    const tx = jateDb.transaction("jate", "rewrite");
+    const store = txt.objectStore("jate");
+    const request = store.put({ content: content});
+    const result = await request;
+    console.log("Data saved to the database", result.value)
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+  console.log("Get from the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  return result;
 
 initdb();
