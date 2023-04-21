39465dbd471072830ff315bd0787135108ee5f68
slurpeesz <khtbee@gmail.com>
2023-04-21 13:48:02 -0700

    added config plugins and css loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,22 +8,62 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
-    plugins: [
-      
-    ],
+    plugins: [new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'JATE'
+    }),
+    new InjectManifest ({
+      swSrc: './src/sw.js',
+      swDest: 'src-sw.js',
+    }),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just Another Text Editor',
+      short_name: 'JATE',
+      description: 'Take notes with Javascript syntax highlighting!',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: '/',
+      publicPath:'/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.?:js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env']
+          ],
+          plugins: ['@babel/plugin-proposal-object-rest-spread','@babel/plugin-transform-runtime'],
+          }
+        }
+       } 
       ],
     },
   };
